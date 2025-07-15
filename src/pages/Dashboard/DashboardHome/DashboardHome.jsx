8191b5e54919875b40ba448cff91b68fc99063ee
MMunim <mmunim9.9.01@gmail.com>
2025-07-15 18:32:48 +0600

    new dashboard home section editing start

--- src/pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -4,68 +4,64 @@ import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Loading from "../../shared/Loading/Loading";
 import useUserRole from "../../../hooks/useUserRole";
+import UserDashboard from "./UserDashboard";
+import RiderDashboard from "./RiderDashboard";
+import AdminDashboard from "./AdminDashboard";
+import Forbidden from "../../Forbidden/Forbidden";
 
 const DashboardHome = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { role } = useUserRole();
+  const { role, isLoading } = useUserRole();
 
-  const { data: dashboardStats = {}, isLoading } = useQuery({
-    queryKey: ["dashboardStats", user?.email],
-    queryFn: async () => {
-      const res = await axiosSecure.get(`/dashboard-stats?email=${user.email}`);
-      return res.data;
-    },
-    enabled: !!user?.email,
-  });
+  if(isLoading){
+    return <Loading></Loading>
+  }
 
-  const statCards = [
-    {
-      title: "Total Parcels",
-      value: dashboardStats.totalParcels || 0,
-      icon: <FaBox className="text-3xl text-blue-600" />,
-    },
-    {
-      title: "Pending Deliveries",
-      value: dashboardStats.pendingDeliveries || 0,
-      icon: <FaTruckMoving className="text-3xl text-yellow-500" />,
-    },
-    {
-      title: "Completed Deliveries",
-      value: dashboardStats.completedDeliveries || 0,
-      icon: <FaCheckCircle className="text-3xl text-green-600" />,
-    },
-    {
-      title: "My Earnings",
-      value: `৳${dashboardStats.totalEarnings || 0}`,
-      icon: <FaMoneyBillWave className="text-3xl text-emerald-500" />,
-    },
-  ];
+  if(role === 'user'){
+    return <UserDashboard></UserDashboard>
+  }
+  else if(role === 'rider'){
+    return <RiderDashboard></RiderDashboard>
+  }
+  else if(role === 'admin'){
+    return <AdminDashboard></AdminDashboard>
+  }
+  else{
+    return <Forbidden></Forbidden>
+  }
 
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl font-semibold mb-6">Welcome Back {role === 'user' ? user?.displayName : role} 👋</h2>
+  // const { data: dashboardStats = {}, isLoading } = useQuery({
+  //   queryKey: ["dashboardStats", user?.email],
+  //   queryFn: async () => {
+  //     const res = await axiosSecure.get(`/dashboard-stats?email=${user.email}`);
+  //     return res.data;
+  //   },
+  //   enabled: !!user?.email,
+  // });
 
-      {isLoading ? (
-        <div><Loading></Loading></div>
-      ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 text-black">
-          {statCards.map((stat, i) => (
-            <div
-              key={i}
-              className="bg-white rounded-xl shadow p-4 flex items-center gap-4 border border-gray-100"
-            >
-              <div className="p-2 rounded-full bg-gray-100">{stat.icon}</div>
-              <div>
-                <p className="text-sm text-gray-500">{stat.title}</p>
-                <h3 className="text-xl font-bold">{stat.value}</h3>
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
+  // const statCards = [
+  //   {
+  //     title: "Total Parcels",
+  //     value: dashboardStats.totalParcels || 0,
+  //     icon: <FaBox className="text-3xl text-blue-600" />,
+  //   },
+  //   {
+  //     title: "Pending Deliveries",
+  //     value: dashboardStats.pendingDeliveries || 0,
+  //     icon: <FaTruckMoving className="text-3xl text-yellow-500" />,
+  //   },
+  //   {
+  //     title: "Completed Deliveries",
+  //     value: dashboardStats.completedDeliveries || 0,
+  //     icon: <FaCheckCircle className="text-3xl text-green-600" />,
+  //   },
+  //   {
+  //     title: "My Earnings",
+  //     value: `৳${dashboardStats.totalEarnings || 0}`,
+  //     icon: <FaMoneyBillWave className="text-3xl text-emerald-500" />,
+  //   },
+  // ];
+}
 
 export default DashboardHome;
